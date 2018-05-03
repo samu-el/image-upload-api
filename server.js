2451ed223e4a8289e5a03901ee815311aa80141a
Samuel Mussie <punisami@gmail.com>
2018-05-03 16:55:49 +0300

    fixed the mongodb url to choose from the env variable or connect to localhost

--- server.js
+++ server.js
@@ -3,16 +3,12 @@ const app = require('./app');
 
 //set port to 3000 if it is not provided
 var port = process.env.PORT || 3000;
-var mongoUrl = process.env.DB;
+var mongoUrl = process.env.DB || "mongodb://localhost/image-upload-api";
 
 //connect to mongodb using the url provided by the environment variables
 //if url is empty exit with status code 1
-if(mongoUrl !== "") {
-    mongoose.connect(mongoUrl);
-} else {
-    console.error("Database url not provided");
-    process.exit(1);
-}
+
+mongoose.connect(mongoUrl);
 
 //start the server and listen on the specified port
 app.listen(port, ()=>{
